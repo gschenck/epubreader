884e450cad507136714a53e4ab7002b96d2af907
Michel Roux <xefir@crystalyx.net>
2023-06-18 02:09:28 +0200

    php7.4 mixed

--- lib/Hooks.php
+++ lib/Hooks.php
@@ -79,7 +79,10 @@ class Hooks {
 		$queryBuilder->executeStatement();
 	}
 
-	private static function isJson(mixed $string): bool {
+	/**
+	 * @param mixed $string
+	 */
+	private static function isJson($string): bool {
 		return is_string($string) && is_array(json_decode($string, true)) && json_last_error() == JSON_ERROR_NONE;
 	}
 }
